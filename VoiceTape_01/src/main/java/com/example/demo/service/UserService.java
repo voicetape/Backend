4e63314ba1a9f957383c39e6f0d45cb9fa00e35b
lauvsong <hyehye9355@naver.com>
2022-12-10 12:52:31 +0900

    [feat] UserService 구현

--- VoiceTape_01/src/main/java/com/example/demo/service/UserService.java
+++ VoiceTape_01/src/main/java/com/example/demo/service/UserService.java
@@ -1,7 +1,33 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class UserService {
+
+    private final UserMapper userMapper;
+
+    public void createUser(User user) {
+        userMapper.createUser(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User getUserByUsername(String username) {
+        return userMapper.getUserByUsername(username);
+    }
+
+
+    public void updateUserByUsername(String username) {
+        userMapper.updateUserByUsername(username);
+    }
+
+    public void deleteUserByUsername(String username) {
+        userMapper.deleteUserByUsername(username);
+    }
 }
