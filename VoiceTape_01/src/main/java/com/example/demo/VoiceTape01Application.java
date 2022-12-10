8cf7d0f4848b681a45141422b976b81a1e1a2ee5
lauvsong <hyehye9355@naver.com>
2022-12-10 12:43:00 +0900

    [chore] 빌드 시 Spring Security 설정 제거
    - 인증 기능 개발 전까지 개발 편의를 위함

--- VoiceTape_01/src/main/java/com/example/demo/VoiceTape01Application.java
+++ VoiceTape_01/src/main/java/com/example/demo/VoiceTape01Application.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 // hihi
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class VoiceTape01Application {
 
 	public static void main(String[] args) {
