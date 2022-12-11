1403e07b11268fc0b167ad68de46a7192f1eb966
lauvsong <hyehye9355@naver.com>
2022-12-11 16:49:13 +0900

    [refactor] Bgm, Voice 객체 enum화
    - 객체 값 보장

--- VoiceTape_01/src/main/java/com/example/demo/entity/Bgm.java
+++ VoiceTape_01/src/main/java/com/example/demo/entity/Bgm.java
@@ -1,15 +1,34 @@
 package com.example.demo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NonNull;
+import lombok.*;
 
-@Data
-@AllArgsConstructor
-public class Bgm {
+import javax.validation.ValidationException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
-	@NonNull private int id;
-	@NonNull private String code;
-	@NonNull private String name;
+@Getter
+@RequiredArgsConstructor
+public enum Bgm implements CodeEnum {
+
+	ADMIN(1, "ROLE_ADMIN", "관리자"),
+	USER(2, "ROLE_USER", "일반 사용자");
+
+	private final int id;
+
+	private final String code;
+	private final String name;
+
+	private static final Map<String, Role> codeMap = new HashMap<>();
+	static {
+		Stream.of(Role.values()).forEach(bgm -> codeMap.put(bgm.getCode(), bgm));
+	}
+
+	@Override
+	public Role getEnumByCode(String code) {
+		return Optional.ofNullable(codeMap.get(code))
+				.orElseThrow(ValidationException::new);
+	}
 
 }
