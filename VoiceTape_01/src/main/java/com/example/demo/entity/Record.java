2dcd2a852c78e4a5cb4d11c092a89f89bb24c5a1
lauvsong <hyehye9355@naver.com>
2022-12-11 16:53:23 +0900

    [fix] Record 엔티티 enum 객체 반영

--- VoiceTape_01/src/main/java/com/example/demo/entity/Record.java
+++ VoiceTape_01/src/main/java/com/example/demo/entity/Record.java
@@ -5,33 +5,22 @@ import java.time.LocalDate;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NonNull;
+import lombok.*;
 
 @Data
-@JsonDeserialize
-@JsonSerialize
-@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Record {
 
 	@NonNull private int id;
 	@NonNull private int userId;
 	
-	private int voiceId;
-	private int bgmId;
+	private Voice voice;
+	private Bgm bgm;
+	private int duration;
 	
 	@NonNull private String url;
 	@NonNull private LocalDate registerDatetime;
-	
-	public Record(int id, int userId, String url, LocalDate registerDatetime)
-	{
-		this.id = id;
-		this.userId = userId;
-		this.url = url;
-		this.registerDatetime = registerDatetime;
-	}
-	
-	
 	
 }
