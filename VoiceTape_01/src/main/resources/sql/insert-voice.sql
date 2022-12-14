c232d0b668fa28e18c85a773aca992dd4760a0e0
lauvsong <hyehye9355@naver.com>
2022-12-14 15:45:27 +0900

    [chore] 변조음성 공통코드 반영

--- VoiceTape_01/src/main/resources/sql/insert-voice.sql
+++ VoiceTape_01/src/main/resources/sql/insert-voice.sql
@@ -1,4 +1,4 @@
-INSERT INTO voicetape.voice (id, code, name) VALUES (1, 'V_BL', '파란 골뱅이');
-INSERT INTO voicetape.voice (id, code, name) VALUES (2, 'V_FE', '여자');
-INSERT INTO voicetape.voice (id, code, name) VALUES (3, 'V_MA', '남자');
-INSERT INTO voicetape.voice (id, code, name) VALUES (4, 'V_IM', '잼민');
+INSERT INTO voicetape.voice (id, code, name) VALUES (1, 'VOICE_BLUE', '파란 골뱅이');
+INSERT INTO voicetape.voice (id, code, name) VALUES (2, 'VOICE_FEMALE', '여자');
+INSERT INTO voicetape.voice (id, code, name) VALUES (3, 'VOICE_MALE', '남자');
+INSERT INTO voicetape.voice (id, code, name) VALUES (4, 'VOICE_KID', '잼민');
