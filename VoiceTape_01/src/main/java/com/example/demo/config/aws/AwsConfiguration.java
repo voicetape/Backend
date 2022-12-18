5c9fc132ffefa95eb373a3d9fa201597856747ea
lauvsong <hyehye9355@naver.com>
2022-12-18 23:36:53 +0900

    [chore] aws credential 지정

--- VoiceTape_01/src/main/java/com/example/demo/config/aws/AwsConfiguration.java
+++ VoiceTape_01/src/main/java/com/example/demo/config/aws/AwsConfiguration.java
@@ -3,7 +3,9 @@ package com.example.demo.config.aws;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
@@ -14,15 +16,20 @@ public class AwsConfiguration {
     private final AwsProperty awsProperty;
 
     @Bean
-    public ProfileCredentialsProvider profileCredentialsProvider() {
-        return ProfileCredentialsProvider.create();
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        return StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(
+                        awsProperty.getAccessKey(),
+                        awsProperty.getSecretKey()
+                )
+        );
     }
 
     @Bean
     public S3Client amazonS3Client() {
         return S3Client.builder()
                 .region(awsProperty.getRegion())
-                .credentialsProvider(profileCredentialsProvider())
+                .credentialsProvider(awsCredentialsProvider())
                 .build();
     }
 
@@ -30,7 +37,7 @@ public class AwsConfiguration {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(awsProperty.getRegion())
-                .credentialsProvider(profileCredentialsProvider())
+                .credentialsProvider(awsCredentialsProvider())
                 .build();
     }
 
