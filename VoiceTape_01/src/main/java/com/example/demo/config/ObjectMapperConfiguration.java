6b3e42e89c0bbe11863d61e5a8115df09b66c387
lauvsong <hyehye9355@naver.com>
2022-12-18 23:45:12 +0900

    [refactor] objectMapper 설정 코드 인라인화

--- VoiceTape_01/src/main/java/com/example/demo/config/ObjectMapperConfiguration.java
+++ VoiceTape_01/src/main/java/com/example/demo/config/ObjectMapperConfiguration.java
@@ -3,6 +3,9 @@ package com.example.demo.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +14,10 @@ public class ObjectMapperConfiguration {
 
     @Bean
     public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, true);
-        objectMapper.setPropertyNamingStrategy(new PropertyNamingStrategies.KebabCaseStrategy());
-        return objectMapper;
+        return new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, true)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .setPropertyNamingStrategy(new PropertyNamingStrategies.KebabCaseStrategy());
     }
 
 }
